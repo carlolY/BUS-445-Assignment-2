2c0c70463fb6227202440138f05da1dc74822e85
carlolY <52217235+carlolY@users.noreply.github.com>
2020-03-26 14:40:45 -0700

    Add files via upload

--- Assignment 2.R
+++ Assignment 2.R
@@ -11,6 +11,7 @@ library("corrplot")
 library("randomForest")
 library("pdp")
 library("ggplot2")
+library(gplots)
 source("BCA_functions_source_file.R")
 
 QK = read_excel("QK_2020.xlsx")
@@ -60,6 +61,11 @@ QK$Title = NULL
 
 QK2 <- na.omit(QK)
 
+corrMatrix <- cor(select_if(QK2, is.numeric)) # see ?dplyr::select_if
+
+corrplot(corrMatrix,method="number",type="lower",
+         diag = FALSE,number.cex = 0.7)
+
 ##### Random Forests #####
 paste(names(QK), collapse = " + ")
 
@@ -77,3 +83,125 @@ QKForestAll[["confusion"]]
 varImpPlot(QKForestAll,type = 2,
            main="WesForestAllv", # title
            cex =0.7) # font size
+
+par(mfrow = c(1,3))
+partial(QKForestAll, pred.var = "NumDeliv", # target and predictor
+        prob = TRUE, # probabilities on yaxis
+        which.class = 2, # predict level 2, "Y"
+        plot = TRUE, # generate plot
+        rug = TRUE, # plot decile hashmarks
+        plot.engine = "ggplot2")
+
+partial(QKForestAll, pred.var = "TotPurch",
+        prob = TRUE, # probabilities on yaxis
+        which.class = 2, # predict level 2, "Y"
+        plot = TRUE, # generate plot
+        rug = T, # plot decile hashmarks
+        plot.engine = "ggplot2")        
+
+partial(QKForestAll, pred.var = "LastOrder",
+        prob = TRUE, # probabilities on yaxis
+        which.class = 2, # predict level 2, "Y"
+        plot = TRUE, # generate plot
+        rug = TRUE, # plot decile hashmarks
+        plot.engine = "ggplot2")
+
+#Trim the top 10%
+QKForestAll.trim <- partial(QKForestAll, pred.var = "NumDeliv",
+                              prob = TRUE,
+                              which.class = 2,
+                              quantiles = TRUE, # prepare data trimming
+                              probs = seq(from = 0.0, to = 0.9, by = 0.02), # of bottom 90%
+                              plot= FALSE) # generate data, no plot
+plotPartial(QKForestAll.trim, # and pass data to plotting function
+            rug = TRUE,
+            train = filter(QK2, Sample == "Estimation"))
+
+QKForestAll.trim <- partial(QKForestAll, pred.var = "TotPurch",
+                            prob = TRUE,
+                            which.class = 2,
+                            quantiles = TRUE, # prepare data trimming
+                            probs = seq(from = 0.0, to = 0.9, by = 0.02), # of bottom 90%
+                            plot= FALSE) # generate data, no plot
+plotPartial(QKForestAll.trim, # and pass data to plotting function
+            rug = TRUE,
+            train = filter(QK2, Sample == "Estimation"))
+
+QKForestAll.trim <- partial(QKForestAll, pred.var = "LastOrder",
+                            prob = TRUE,
+                            which.class = 2,
+                            quantiles = TRUE, # prepare data trimming
+                            probs = seq(from = 0.0, to = 0.9, by = 0.02), # of bottom 90%
+                            plot= FALSE) # generate data, no plot
+plotPartial(QKForestAll.trim, # and pass data to plotting function
+            rug = TRUE,
+            train = filter(QK2, Sample == "Estimation"))
+
+
+##### Logistic Regression #####
+QK$SUBSCRIBE.num = ifelse(QK$SUBSCRIBE == "N", 0,1)
+QK$DA_Income.Cat <- binVariable(QK$DA_Income, bins = 10,
+                             method = "interval",
+                             labels = NULL)
+QK$NumDeliv.Cat <- binVariable(QK$NumDeliv, bins = 10,
+                                method = "interval",
+                                labels = NULL)
+
+
+plotmeans(SUBSCRIBE.num ~ DA_Income.Cat, data = QK)
+plotmeans(SUBSCRIBE.num ~ NumDeliv.Cat, data = QK)
+
+QK2$DA_Income2 = I(QK2$DA_Income^2)
+QK2$NumDeliv2 = I(QK2$NumDeliv^2)
+
+QK.log = glm(SUBSCRIBE ~ Disc + LastOrder + DA_Income2 + DA_Under20 + DA_Over60 + 
+            DA_Single + NumDeliv2 + Healthy + Veggie + Meaty + Special + 
+            TotPurch + MealsPerDlv, 
+            data = filter(QK2, Sample =="Estimation"),
+            family = binomial(logit))
+summary(QK.log)
+
+
+# Calculate and print McFadden R square (See Logistic Regression Chapter)
+paste("McFadden R^2:", round(1 - (QK.log$deviance / QK.log$null.deviance),3))
+
+#Run a stepwise regression using the "Weslogis" model
+QKStep <- step(QK.log, direction = "both")
+summary(QKStep)
+
+## Lift Chart
+lift.chart(modelList = c("QK.log", "QKStep"),
+           data = filter(QK2, Sample == "Validation"),
+           targLevel = "Y", trueResp = 0.01, type = "cumulative",
+           sub = "Validation")
+
+# Compare forests and regression models
+lift.chart(modelList = c("QKStep","QKForestAll"),
+           data = filter(QK2, Sample == "Validation"),
+           targLevel = "Y", trueResp = 0.01, type = "cumulative",
+           sub = "Validation")
+
+
+##### Neural Network #####
+# 4 node nnet
+QKNnet4 <- Nnet(formula = SUBSCRIBE ~ Disc + LastOrder + DA_Income + DA_Under20 + 
+                  NumDeliv + Healthy + Meaty + Special + MealsPerDlv, 
+                data = filter(QK2, Sample == "Estimation"),
+                 decay = 0.15, size = 4)
+
+lift.chart(modelList = c("QKStep","QKNnet4"),
+           data = filter(QK2, Sample == "Validation"),
+           targLevel = "Y", trueResp = 0.01, type = "cumulative",
+           sub = "Validation")
+
+## All variables in nnet
+QKNetAllv <- Nnet(formula = SUBSCRIBE ~ Disc + LastOrder + DA_Income + DA_Under20 + 
+                    DA_Over60 + DA_Single + NumDeliv + NumMeals + Healthy + Veggie + 
+                    Meaty + Special + TotPurch + Sample + MealsPerDlv,
+                   data = filter(QK2, Sample =="Estimation"),
+                   decay = 0.15, size = 4)
+
+lift.chart(modelList = c("QKStep","QKNetAllv"),
+           data = filter(QK2, Sample == "Validation"),
+           targLevel = "Y", trueResp = 0.01, type = "cumulative",
+           sub = "Validation")
